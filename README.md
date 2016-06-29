97cc738686cbf95e4cb9b7d0dc9f597f8280d61e
John <rbcs.john@gmail.com>
2016-06-29 12:43:41 +0200

    Modified both README and .gitignore and added setup file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # registry-inspector
 A tool for inspecting the contents of your a registry.
+
+This tool enables the user to view information about each image running in the registry as well as layer information.
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='registry-inspector',
+	version='1.0.0',
+	description='A tool for inspecting the contents of your docker registry',
+	#long_description=
+	#url=
+	author='John Odetokun',
+	author_email='rbcs.john@gmail.com',
+	#license=
+	classifiers=[
+	'Development Status :: 2 - Pre-Alpha',
+	'Intended Audience :: SRE team',
+	'Topic :: Docker registry :: inspector',
+	#'License ::
+	'Programming Language :: Python :: 2',
+	'Programming Language :: Python :: 2.7',
+	],
+	keywords='Docker registry image layer inspector',
+	packages=find_packages(),
+	#nstall_requires=[''],
+	#extras_require={},
+	#package_data={},
+	#data_files[]
+	#entry_points={},
+
+
+)
