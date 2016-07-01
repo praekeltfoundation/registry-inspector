f1d55fe667b16b592036dfdd05c494c3221f528b
John <rbcs.john@gmail.com>
2016-07-01 14:40:40 +0200

    real implementation

--- registry_inspector/registry_client.py
+++ registry_inspector/registry_client.py
@@ -1,11 +1,16 @@
+import requests
+
+
 class RegistryClient(object):
     def __init__(self, registry):
         self.registry = registry
 
+    def _call_registry(self, method, path):
+        return requests.request(method, self.registry + path, verify=False)
+
     def get_catalog(self):
         # TODO: Replace this with a real implementation
-        x = {'repositories': 'cool'}
-        return x
+        return self._call_registry('GET', '/v2/_catalog/').json()
 
     def get_tags(self, name):
         x = {'tags': 'cool'}
--- registry_inspector/tests/test_registry_client.py
+++ registry_inspector/tests/test_registry_client.py
@@ -2,20 +2,20 @@ from registry_inspector.registry_client import RegistryClient
 
 
 def test_get_catalog():
-    registry = 'https://my.registry.local:5000'
+    registry = 'http://localhost:5000'
     client = RegistryClient(registry)
     assert ('repositories' in client.get_catalog())
 
 
 def test_get_tags():
-    registry = 'https://my.registry.local:5000'
+    registry = 'http://localhost:5000'
     client = RegistryClient(registry)
     name = 'ubuntu'
     assert ('tags' in client.get_tags(name))
 
 
 def test_get_manifests():
-    registry = 'https://my.registry.local:5000'
+    registry = 'http://localhost:5000'
     client = RegistryClient(registry)
     name = 'ubuntu'
     tag = 'latest'
