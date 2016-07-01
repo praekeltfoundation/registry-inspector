86dd482426350acc2f60cbf2f7ae86b9ec5cfeab
John <rbcs.john@gmail.com>
2016-07-01 10:50:39 +0200

    get_tags test

--- registry_inspector/registry_client.py
+++ registry_inspector/registry_client.py
@@ -2,3 +2,7 @@ def get_catalog(registry):
 	#TODO: Replace this with a real implementation
     x = {'repositories':'cool'}
     return x
+
+def get_tags(registry, name):
+	x = {'tags' : 'cool'}
+	return x
--- registry_inspector/tests/test_registry_client.py
+++ registry_inspector/tests/test_registry_client.py
@@ -1,5 +1,11 @@
 from registry_inspector.registry_client import get_catalog
+from registry_inspector.registry_client import get_tags
 
 def test_get_catalog():
 	registry = 'https://my.registry.local:5000'
 	assert ('repositories' in get_catalog(registry))
+
+def test_get_tags():
+	registry = 'https://my.registry.local:5000'
+	name = 'ubuntu'
+	assert ('tags' in get_tags(registry, name))
