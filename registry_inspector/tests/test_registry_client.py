0733d7e3d90e8de3fef141528a5eaf58bd4a7c8a
John <rbcs.john@gmail.com>
2016-07-06 18:13:11 +0200

    flake8 changes

--- registry_inspector/tests/test_registry_client.py
+++ registry_inspector/tests/test_registry_client.py
@@ -35,38 +35,30 @@ def test_get_manifests():
     responses.add(
         responses.GET, registry + '/v2/%s/manifests/%s' %
         (name, tag,), content_type='application/json',
-        json={"fsLayers": [
-      {
-         "blobSum": "sha256:a3ed95caeb02ffe68cdd9fd84406680ae93d633cb16422d00e8a7c22955b46d4"
-      },
-      {
-         "blobSum": "sha256:10fb34ebccea88897d4b570120719b23fcbf3e556abdb79fdba6b2e0e4bad9ab"
-      },
-      {
-         "blobSum": "sha256:4c876570bd7d10c58fc291fa980404290de01bfc01410daeb57a9dd6b86fac57"
-      },
-      {
-         "blobSum": "sha256:6874f9870f5f8b13aea44707fddf746825247dda0f3abc9d93438b58c97cdacd"
-      },
-      {
-         "blobSum": "sha256:5ba4f30e5bea63dcc2e7054b8b4f41ab1e5fcc7db0a88fc79359b890bcfe2258"
-      }
-   ]})
+        json={"fsLayers": [{"blobSum": "sha256:a3ed95caeb02ffe68cdd9fd844066"
+                                       "80ae93d633cb1642d00e8a7c22955b46d4"},
+                           {"blobSum": "sha256:10fb34ebccea88897d4b570120719"
+                                       "b23fcbf3e556abdb79fdba6b2e0e4bad9ab"},
+                           {"blobSum": "sha256:4c876570bd7d10c58fc291fa98040"
+                                       "4290de01bfc01410daeb57a9dd6b86fac57"},
+                           {"blobSum": "sha256:6874f9870f5f8b13aea44707fddf7"
+                                       "46825247dda0f3abc9d93438b58c97cdacd"},
+                           {"blobSum": "sha256:5ba4f30e5bea63dcc2e7054b8b4f4"
+                                       "1ab1e5fcc7db0a88fc7"
+                                       "9359b890bcfe2258"}]})
     resp = client.get_manifests(name, tag)['fsLayers']
-    assert resp == {"fsLayers": [
-      {
-         "blobSum": "sha256:a3ed95caeb02ffe68cdd9fd84406680ae93d633cb16422d00e8a7c22955b46d4"
-      },
-      {
-         "blobSum": "sha256:10fb34ebccea88897d4b570120719b23fcbf3e556abdb79fdba6b2e0e4bad9ab"
-      },
-      {
-         "blobSum": "sha256:4c876570bd7d10c58fc291fa980404290de01bfc01410daeb57a9dd6b86fac57"
-      },
-      {
-         "blobSum": "sha256:6874f9870f5f8b13aea44707fddf746825247dda0f3abc9d93438b58c97cdacd"
-      },
-      {
-         "blobSum": "sha256:5ba4f30e5bea63dcc2e7054b8b4f41ab1e5fcc7db0a88fc79359b890bcfe2258"
-      }
-   ]}['fsLayers']
+    assert resp == {"fsLayers": [{"blobSum": "sha256:a3ed95caeb02ffe68cdd9fd"
+                                             "84406680ae93d633cb1642d00e8a7c"
+                                             "22955b46d4"},
+                                 {"blobSum": "sha256:10fb34ebccea88897d4b570"
+                                             "120719b23fcbf3e556abdb79fdba6b"
+                                             "2e0e4bad9ab"},
+                                 {"blobSum": "sha256:4c876570bd7d10c58fc291f"
+                                             "a980404290de01bfc01410daeb57a9"
+                                             "dd6b86fac57"},
+                                 {"blobSum": "sha256:6874f9870f5f8b13aea4470"
+                                             "7fddf746825247dda0f3abc9d93438"
+                                             "b58c97cdacd"},
+                                 {"blobSum": "sha256:5ba4f30e5bea63dcc2e7054"
+                                             "b8b4f41ab1e5fcc7db0a88fc79359b"
+                                             "890bcfe2258"}]}['fsLayers']
