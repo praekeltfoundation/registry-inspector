f830287d367f25ecf8cfc68a4812e9bd8ff661f8
John <rbcs.john@gmail.com>
2016-07-11 11:25:29 +0200

    small fixes

--- registry_inspector/tests/test_registry_client.py
+++ registry_inspector/tests/test_registry_client.py
@@ -67,26 +67,24 @@ def test_get_manifests():
 def test_get_digest_length():
     def headers_callback(request):
         headers = {
-                            "Accept-Ranges": "bytes",
-                            "Cache-Control": "max-age=31536000",
-                            "Content-Length": "32",
-                            "Content-Type": "application/octet-stream",
-                            "Docker-Content-Digest": "sha256:a3ed95caeb02ffe6"
-                                                     "8cdd9fd84406680ae93d633"
-                                                     "cb16422d00e8a7c"
-                                                     "22955b46d4",
-                            "Docker-Distribution-Api-Version": "registry/2.0",
-                            "Etag": "sha256: a3ed95caeb02ffe68cdd9fd84406680a"
-                                    "e93d633cb16422d00e8a7c22955b46d4",
-                            "X-Content-Type-Options": "nosniff",
-                            "Date": "Thu, 07 Jul 2016 13:14:44 GMT"}
+            "Accept-Ranges": "bytes",
+            "Cache-Control": "max-age=31536000",
+            "Content-Length": "32",
+            "Content-Type": "application/octet-stream",
+            "Docker-Content-Digest": "sha256:a3ed95caeb02ffe68cdd9fd84406680a"
+                                     "e93d633cb16422d00e8a7c22955b46d4",
+            "Docker-Distribution-Api-Version": "registry/2.0",
+            "Etag": "sha256: a3ed95caeb02ffe68cdd9fd84406680a"
+                    "e93d633cb16422d00e8a7c22955b46d4",
+            "X-Content-Type-Options": "nosniff",
+            "Date": "Thu, 07 Jul 2016 13:14:44 GMT"}
         body = ""
         return 200, headers, body
     registry = 'http://localhost:5000'
     client = RegistryClient(registry)
     name = 'ubuntu'
-    digest = 'sha256:a3ed95caeb02ffe68cdd9fd84406680ae93d633' + \
-             'cb16422d00e8a7c22955b46d4'
+    digest = ('sha256:a3ed95caeb02ffe68cdd9fd84406680ae93d633'
+              'cb16422d00e8a7c22955b46d4')
 
     responses.add_callback(
         responses.HEAD, registry + '/v2/%s/blobs/%s' %
@@ -101,4 +99,4 @@ def test_log(capsys):
     client = RegistryClient(registry)
     client.log("log testing")
     out, err = capsys.readouterr()
-    assert err == "log testing\n"
+    assert (out, err) == ("", "log testing\n")
