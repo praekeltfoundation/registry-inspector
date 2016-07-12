18670b271cca4f1a596c44f2219aab55b90aa796
John <rbcs.john@gmail.com>
2016-07-12 11:10:45 +0200

    few fixes

--- registry_inspector/inspector.py
+++ registry_inspector/inspector.py
@@ -5,6 +5,8 @@ class RegistryInspector(object):
     def __init__(self, registry):
         self.registry = registry
 
+    # function returns a dict with blob digests as keys and their
+    # lengths as values
     def get_layer_dict(self, image, manifest):
         client = RegistryClient(self.registry)
         layer_dict = {}
@@ -12,3 +14,12 @@ class RegistryInspector(object):
         for value in blobSumValues:
             layer_dict[value] = int(client.get_digest_length(image, value))
         return layer_dict
+
+    # function returns a dict with tags as keys and their
+    # manifests as values
+    def get_tag_manifests(self, image, tag_list):
+        client = RegistryClient(self.registry)
+        manifest_dict = {}
+        for tag in tag_list:
+            manifest_dict[tag] = client.get_manifest(image, tag)
+        return manifest_dict
--- registry_inspector/tests/test_inspector.py
+++ registry_inspector/tests/test_inspector.py
@@ -3,39 +3,32 @@ import responses
 
 
 def layer_callback(sha256, content_length):
-        def cb(request):
-            headers = {
-                    "Docker-Content-Digest": "sha256:%s" % (sha256,),
-                    "Etag": "sha256: %s" % (sha256,),
-                    "Content-Length": content_length,
-            }
-            return 200, headers, ""
-        return cb
+    def cb(request):
+        headers = {
+                "Docker-Content-Digest": "sha256:%s" % (sha256,),
+                "Etag": "sha256: %s" % (sha256,),
+                "Content-Length": content_length,
+        }
+        return 200, headers, ""
+    return cb
 
 
 @responses.activate
 def test_get_layer_dict():
-
+    BLOB1 = 'a3ed95caeb02ffe68cdd9fd84406680ae93d633cb16422d00e8a7c22955b46d4'
+    BLOB2 = '10fb34ebccea88897d4b570120719b23fcbf3e556abdb79fdba6b2e0e4bad9ab'
     registry = 'http://localhost:5000'
-    name = 'ubuntu'
-    digest = {('a3ed95caeb02ffe68cdd9fd84406680ae93d633'
-              'cb16422d00e8a7c22955b46d4'): '32', ('10fb34ebccea88897d4b5701'
-              '20719b23fcbf3e556abdb79fdba6b2e0e4bad9ab'): '680'}
+    image = 'ubuntu'
+    digest = {BLOB1: '32', BLOB2: '680'}
 
     for digest_value in digest:
         responses.add_callback(
             responses.HEAD, registry + '/v2/%s/blobs/%s' %
-            (name, 'sha256:'+digest_value,),
+            (image, 'sha256:'+digest_value,),
             callback=layer_callback(digest_value, digest[digest_value]))
 
-    manifest = {"fsLayers": [{"blobSum": "sha256:a3ed95caeb02ffe68cdd9fd84"
-                                         "406680ae93d633cb16422d00e8a7c229"
-                                         "55b46d4"},
-                             {"blobSum": "sha256:10fb34ebccea88897d4b57012"
-                                         "0719b23fcbf3e556abdb79fdba6b2e0e"
-                                         "4bad9ab"}]}
+    manifest = {"fsLayers": [{"blobSum": "sha256:"+BLOB1},
+                             {"blobSum": "sha256:"+BLOB2}]}
     inspector = RegistryInspector(registry)
     resp = inspector.get_layer_dict('ubuntu', manifest)
-    assert resp == {"sha256:a3ed95caeb02ffe68cdd9fd84406680ae93d633cb16422"
-                    "d00e8a7c22955b46d4": 32, "sha256:10fb34ebccea88897d4b57"
-                    "0120719b23fcbf3e556abdb79fdba6b2e0e4bad9ab": 680}
+    assert resp == {"sha256:"+BLOB1: 32, "sha256:"+BLOB2: 680}
