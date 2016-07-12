4c34aecd7aedea8df0b319bd12850322e5d3fc4b
John <rbcs.john@gmail.com>
2016-07-12 17:14:39 +0200

    Command line tool added

--- registry_inspector/inspector.py
+++ registry_inspector/inspector.py
@@ -1,4 +1,4 @@
-from registry_inspector.registry_client import RegistryClient
+from registry_client import RegistryClient
 
 
 class RegistryInspector(object):
@@ -9,17 +9,21 @@ class RegistryInspector(object):
     # lengths as values
     def get_layer_dict(self, image, manifest):
         client = RegistryClient(self.registry)
+        client.log('Creating layer dictionary')
         layer_dict = {}
         blobSumValues = [layer['blobSum'] for layer in manifest['fsLayers']]
         for value in blobSumValues:
             layer_dict[value] = int(client.get_digest_length(image, value))
+        client.log('layer dictionary created')
         return layer_dict
 
     # function returns a dict with tags as keys and their
     # manifests as values
     def get_tag_manifests(self, image, tag_list):
         client = RegistryClient(self.registry)
+        client.log('Creating manifest dictionary')
         manifest_dict = {}
         for tag in tag_list:
             manifest_dict[tag] = client.get_manifest(image, tag)
+        client.log('Manifest dictionary created')
         return manifest_dict
